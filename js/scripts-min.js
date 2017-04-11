6ba787229c1bca2a7b1e328fc6d5bb02d085f5af
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-04-11 09:16:54 -0700

    Caught up
    * caught up to before I had issues!
    * sounds are linked to keys
    * need to add my own key as well

--- js/scripts-min.js
+++ js/scripts-min.js
@@ -1,1 +1,1 @@
-console.log("Hello World");
+window.addEventListener("keydown",function(e){console.log(e.keyCode)});
--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 // Scripts //
 
-console.log('Hello World');
+window.addEventListener('keydown', function (e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return; // stop the function from running all together
+
+    audio.currentTime = 0; // rewind to the start
+    audio.play();
+
+    console.log(key)
+});
