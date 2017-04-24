62763ba0f7c2e857d28e97ffb5786892665b53df
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-04-23 17:24:37 -0700

    YAY.
    *couldn’t use JS video to properly create the removeClass.
    *used setTimeout instead!

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,23 @@
-// Scripts //
+console.log('1,2,3...');
 
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+  }
 
-
-window.addEventListener('keydown', function (e) {
+  function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    
-    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
 
-    if (!audio) return; // stop the function from running all together
-
-    audio.currentTime = 0; // rewind to the start
+    audio.currentTime = 0;
     audio.play();
+    key.classList.add('playing');
+    setTimeout(function(){ 
+        key.classList.remove('playing'); }, 100   
+      );
+  };
 
-//    key.classList.add('playing');
-     
-});
-
-//function removeTransition(e) {
-// if(e.propertyName !== 'transform') return; //skip it if it is not a transform 
-}
-
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => key.addEventListener('transitionend', removeTransition ));
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  window.addEventListener('keydown', playSound);
