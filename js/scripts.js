8ab9a3283be4374c6f66cb68182993a79d09c26f
Olivia Davies <olivia.davies@eagles.ewu.edu>
2017-06-16 20:45:52 -0700

    THIS IS COOL
    I’M SO DOWN FOR DOING THINGS LIKE THIS. THIs is so fun but I am
    exhausted so I will keep working!

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-console.log('1,2,3...');
+//console.log('1,2,3...');
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-  }
+}
 
-  function playSound(e) {
+function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
@@ -13,11 +13,27 @@ function removeTransition(e) {
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
-    setTimeout(function(){ 
-        key.classList.remove('playing'); }, 100   
-      );
-  }
+    setTimeout(function () {
+        key.classList.remove('playing');
+    }, 100);
+}
 
-  const keys = Array.from(document.querySelectorAll('.key'));
-  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-  window.addEventListener('keydown', playSound);
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+
+
+function setRandomColor(e) {
+    $(".playing").css("border", getRandomColor());
+}
+
+window.addEventListener('keydown', setRandomColor());
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
